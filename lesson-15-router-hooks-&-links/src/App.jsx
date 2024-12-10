3d24a933ca8e623a12088a63558fad353b889dcc
Tri Ngo <tringo831@gmail.com>
2024-12-09 16:43:26 -0800

    Update App.jsx and Main.jsx

--- lesson-15-router-hooks-&-links/src/App.jsx
+++ lesson-15-router-hooks-&-links/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { format } from 'date-fns';
 import Header from './Header';
--- lesson-15-router-hooks-&-links/src/main.jsx
+++ lesson-15-router-hooks-&-links/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // Router should only wrap the entire app once in main.jsx
 createRoot(document.getElementById('root')).render(
